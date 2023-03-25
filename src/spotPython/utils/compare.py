d562ec0575b1e721ebec0c7df21c1ee9b88bae78
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2023-03-25 15:52:39 +0100

    levels management

--- src/spotPython/utils/compare.py
+++ src/spotPython/utils/compare.py
@@ -18,3 +18,27 @@ def selectNew(A, X, tolerance=0):
         B = np.abs(A - X[i, :])
         ind = ind + np.all(B <= tolerance, axis=1)
     return A[~ind], ~ind
+
+
+def find_equal_in_lists(a, b):
+    """Find equal values in two lists.
+
+    Args:
+        a (list): list with a values
+        b (list): list with b values
+
+    Returns:
+        list: list with 1 if equal, otherwise 0
+    Example:
+        >>> a = [1, 2, 3, 4, 5]
+        >>> b = [1, 2, 3, 4, 5]
+        >>> find_equal_in_lists(a, b)
+        [1, 1, 1, 1, 1]
+    """
+    equal = []
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            equal.append(1)
+        else:
+            equal.append(0)
+    return equal
