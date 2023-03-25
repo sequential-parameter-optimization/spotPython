6751b2884dd06a589508fffb0f935af2b0ade5d2
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2023-03-26 00:23:00 +0100

    v0.0.17: consider "float" as non factors

--- src/spotPython/utils/repair.py
+++ src/spotPython/utils/repair.py
@@ -5,13 +5,14 @@ from numpy import isfinite
 def repair_non_numeric(X, var_type):
     """
     Round non-numeric values to integers.
+    This applies to all variables except for "num" and "float".
 
     Args:
         X (numpy.ndarray): X array
         var_type (list): list with type information
     """
     for i in range(X.shape[1]):
-        if var_type[i] != "num":
+        if var_type[i] not in ["num", "float"]:
             X[:, i] = around(X[:, i])
     return X
 
