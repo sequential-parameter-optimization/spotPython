689fca81faf38177cbcad66f978ee8b9e442ae25
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2023-04-09 22:29:29 +0200

    v0.0.28
    fix "pandas >= 2.0" error in aggregate_mean()

--- src/spotPython/utils/aggregate.py
+++ src/spotPython/utils/aggregate.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 
-
 def aggregate_mean_var(X, y, sort=False):
     """
     Aggregate array to mean.
@@ -9,20 +8,32 @@ def aggregate_mean_var(X, y, sort=False):
     Args:
         X (numpy.ndarray): X array, shape `(n, k)`.
         y (numpy.ndarray): values, shape `(n,)`.
+        sort (bool): Whether to sort the resulting DataFrame by the group keys.
 
     Returns:
-        (numpy.ndarray): aggregated `X` values, shape `(n-m, k)`, if `m`duplicates in `X`.
-        (numpy.ndarray): aggregated (mean per group) `y` values, shape `(1,)`, if `m`duplicates in `X`.
-        (numpy.ndarray): aggregated (variance per group) `y` values, shape `(1,)`, if `m`duplicates in `X`.
+        (numpy.ndarray): aggregated `X` values, shape `(n-m, k)`, if `m` duplicates in `X`.
+        (numpy.ndarray): aggregated (mean per group) `y` values, shape `(1,)`, if `m` duplicates in `X`.
+        (numpy.ndarray): aggregated (variance per group) `y` values, shape `(1,)`, if `m` duplicates in `X`.
     """
-    df = pd.DataFrame(X, dtype=pd.Float64Dtype)
-    # df.columns=["X"+str(i) for i in range(df.shape[1])]
-    df = df.assign(y=y)
-    df_m = df.groupby(list(df.columns.difference(["y"])), as_index=False, sort=sort).mean()
-    df_var = df.groupby(list(df.columns.difference(["y"])), as_index=False, sort=sort).var()
-    A = df_m.to_numpy(dtype=np.float64)
-    B = df_var.to_numpy()
-    return np.delete(A, -1, 1), A[:, -1], B[:, -1]
+    # Create a DataFrame from X and y
+    df = pd.DataFrame(X)
+    df["y"] = y
+
+    # Group by all columns except 'y' and calculate the mean and variance of 'y' for each group
+    grouped = df.groupby(list(df.columns.difference(["y"])), as_index=False, sort=sort)
+    df_mean = grouped.mean()
+    df_var = grouped.var()
+
+    # Convert the resulting DataFrames to numpy arrays
+    mean_array = df_mean.to_numpy()
+    var_array = df_var.to_numpy()
+
+    # Split the resulting arrays into separate arrays for X and y
+    X_agg = np.delete(mean_array, -1, 1)
+    y_mean = mean_array[:, -1]
+    y_var = var_array[:, -1]
+
+    return X_agg, y_mean, y_var
 
 
 def get_ranks(x):
