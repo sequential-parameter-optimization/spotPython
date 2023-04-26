24c0a68bb0c98407d6ad77726bcf4066347de527
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2023-04-26 12:55:01 +0200

    v0.0.41

--- src/spotPython/fun/hypertorch.py
+++ src/spotPython/fun/hypertorch.py
@@ -3,6 +3,12 @@ import numpy as np
 from numpy import array
 from sklearn.pipeline import make_pipeline
 from spotPython.utils.convert import get_Xy_from_df
+from spotPython.utils.data import load_data
+import torch.nn as nn
+import torch.optim as optim
+import torch
+import os
+from torch.utils.data import random_split
 
 
 from spotPython.hyperparameters.values import assign_values
@@ -64,23 +70,100 @@ class HyperSklearn:
             raise Exception
 
     def evaluate_model(self, model, fun_control):
+        # TODO: config anpassen
         try:
+            lr = fun_control["lr"]
+            checkpoint_dir = fun_control["checkpoint_dir"]
+            data_dir = fun_control["data_dir"]
+
             X_train, y_train = get_Xy_from_df(fun_control["train"], fun_control["target_column"])
             X_test, y_test = get_Xy_from_df(fun_control["test"], fun_control["target_column"])
             model.fit(X_train, y_train)
-            
-            
-            
-            
-            
             df_preds = model.predict(X_test)
             df_eval = fun_control["metric_sklearn"](y_test, df_preds)
+            #
+            device = "cpu"
+            # if torch.cuda.is_available():
+            #     device = "cuda:0"
+            #     if torch.cuda.device_count() > 1:
+            #         net = nn.DataParallel(net)
+            model.to(device)
+
+            criterion = nn.CrossEntropyLoss()
+            optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+            if checkpoint_dir:
+                model_state, optimizer_state = torch.load(os.path.join(checkpoint_dir, "checkpoint"))
+                model.load_state_dict(model_state)
+                optimizer.load_state_dict(optimizer_state)
+
+            trainset, testset = load_data(data_dir)
+
+            test_abs = int(len(trainset) * 0.8)
+            train_subset, val_subset = random_split(trainset, [test_abs, len(trainset) - test_abs])
+
+            trainloader = torch.utils.data.DataLoader(
+                train_subset, batch_size=int(config["batch_size"]), shuffle=True, num_workers=8
+            )
+            valloader = torch.utils.data.DataLoader(
+                val_subset, batch_size=int(config["batch_size"]), shuffle=True, num_workers=8
+            )
+
+            for epoch in range(10):  # loop over the dataset multiple times
+                running_loss = 0.0
+                epoch_steps = 0
+                for i, data in enumerate(trainloader, 0):
+                    # get the inputs; data is a list of [inputs, labels]
+                    inputs, labels = data
+                    inputs, labels = inputs.to(device), labels.to(device)
+
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+
+                    # forward + backward + optimize
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
+
+                    # print statistics
+                    running_loss += loss.item()
+                    epoch_steps += 1
+                    if i % 2000 == 1999:  # print every 2000 mini-batches
+                        print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / epoch_steps))
+                        running_loss = 0.0
+
+                # Validation loss
+                val_loss = 0.0
+                val_steps = 0
+                total = 0
+                correct = 0
+                for i, data in enumerate(valloader, 0):
+                    with torch.no_grad():
+                        inputs, labels = data
+                        inputs, labels = inputs.to(device), labels.to(device)
+
+                        outputs = model(inputs)
+                        _, predicted = torch.max(outputs.data, 1)
+                        total += labels.size(0)
+                        correct += (predicted == labels).sum().item()
+
+                        loss = criterion(outputs, labels)
+                        val_loss += loss.cpu().numpy()
+                        val_steps += 1
+
+                # TODO:
+                # with tune.checkpoint_dir(epoch) as checkpoint_dir:
+                path = os.path.join(checkpoint_dir, "checkpoint")
+                torch.save((model.state_dict(), optimizer.state_dict()), path)
+            df_eval = val_loss / val_steps
+            df_preds = np.nan
+            # accuracy = correct / total
         except Exception as err:
             print(f"Error in fun_sklearn(). Call to evaluate_model failed. {err=}, {type(err)=}")
             df_eval = np.nan
-            df_eval = np.nan
+            df_preds = np.nan
         return df_eval, df_preds
-  
 
     def get_sklearn_df_eval_preds(self, model):
         try:
@@ -92,7 +175,7 @@ class HyperSklearn:
             df_preds = np.nan
         return df_eval, df_preds
 
-    def fun_sklearn(self, X, fun_control=None):
+    def fun_torch(self, X, fun_control=None):
         z_res = np.array([], dtype=float)
         self.fun_control.update(fun_control)
         self.check_X_shape(X)
--- src/spotPython/torch/net.py
+++ src/spotPython/torch/net.py
@@ -1,8 +1,10 @@
 from torch import nn
+import torch.nn.functional as F
+
 
 class Net_CIFAR10(nn.Module):
     def __init__(self, l1=120, l2=84):
-        super(Net, self).__init__()
+        super(Net_CIFAR10, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
@@ -18,4 +20,3 @@ class Net_CIFAR10(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-        
