58cee0c6f772aeeb4763263191daca453214f3ef
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2023-04-26 12:55:01 +0200

    v0.0.41

---
+++ src/spotPython/utils/file.py
@@ -0,0 +1,12 @@
+import torchvision
+import torchvision.transforms as transforms
+
+
+def load_data(data_dir="./data"):
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True, download=True, transform=transform)
+
+    testset = torchvision.datasets.CIFAR10(root=data_dir, train=False, download=True, transform=transform)
+
+    return trainset, testset
