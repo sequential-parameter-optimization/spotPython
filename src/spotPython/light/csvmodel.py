93e054670084b815d880c918cba5d425cfecbf63
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2023-06-25 18:05:55 +0200

    dropout

--- src/spotPython/light/csvmodel.py
+++ src/spotPython/light/csvmodel.py
@@ -1,5 +1,3 @@
-import os
-
 import lightning as L
 import torch
 import torch.nn.functional as F
@@ -8,12 +6,10 @@ from torchmetrics.functional import accuracy
 from spotPython.torch.mapk import MAPK
 
 
-PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
-BATCH_SIZE = 256 if torch.cuda.is_available() else 64
-
-
 class CSVModel(L.LightningModule):
-    def __init__(self, l1, epochs, batch_size, act_fn, optimizer, learning_rate=2e-4, _L_in=64, _L_out=11):
+    def __init__(
+        self, l1, epochs, batch_size, act_fn, optimizer, dropout_prob, learning_rate=2e-4, _L_in=64, _L_out=11
+    ):
         super().__init__()
 
         # We take in input dimensions as parameters and use those to dynamically build model.
@@ -26,6 +22,7 @@ class CSVModel(L.LightningModule):
         self.batch_size = batch_size
         self.act_fn = act_fn
         self.optimizer = optimizer
+        self.dropout_prob = dropout_prob
         self.learning_rate = learning_rate
         self.train_mapk = MAPK(k=3)
         self.valid_mapk = MAPK(k=3)
@@ -47,7 +44,7 @@ class CSVModel(L.LightningModule):
         layer_sizes = [_L_in] + hidden_sizes
         layer_size_last = layer_sizes[0]
         for layer_size in layer_sizes[1:]:
-            layers += [nn.Linear(layer_size_last, layer_size), act_fn]
+            layers += [nn.Linear(layer_size_last, layer_size), act_fn, nn.Dropout(self.dropout_prob)]
             layer_size_last = layer_size
         layers += [nn.Linear(layer_sizes[-1], self._L_out)]
         # nn.Sequential summarizes a list of modules into a single module, applying them in sequence
@@ -62,12 +59,9 @@ class CSVModel(L.LightningModule):
         logits = self(x)
         # compute cross entropy loss from logits and y
         loss = F.cross_entropy(logits, y)
-        self.train_mapk(logits, y)
-        self.log("train_mapk", self.train_mapk, on_step=True, on_epoch=False)
+        # self.train_mapk(logits, y)
+        # self.log("train_mapk", self.train_mapk, on_step=True, on_epoch=False)
         return loss
-
-    def training_epoch_end(self, outputs):
-        self.train_mapk.reset()
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
@@ -78,7 +72,7 @@ class CSVModel(L.LightningModule):
         preds = torch.argmax(logits, dim=1)
         acc = accuracy(preds, y, task="multiclass", num_classes=self._L_out)
         self.valid_mapk(logits, y)
-        self.log("valid_mapk", self.valid_mapk, on_step=True, on_epoch=True)
+        self.log("valid_mapk", self.valid_mapk, on_step=False, on_epoch=True)
         self.log("val_loss", loss, prog_bar=True)
         self.log("val_acc", acc, prog_bar=True)
 
