c7e9ea99f3fe374192a7160375ce1741d66b0d48
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2023-10-31 18:01:46 +0100

    tensorboard process handling

---
+++ src/spotPython/utils/tensorboard.py
@@ -0,0 +1,32 @@
+import subprocess
+
+
+def start_tensorboard() -> subprocess.Popen:
+    """Starts a tensorboard server in the background.
+
+    Returns:
+        process: The process of the tensorboard server.
+
+    Examples:
+        >>> process = start_tensorboard()
+
+    """
+    cmd = ["tensorboard", "--logdir=./runs"]
+    process = subprocess.Popen(cmd)
+    return process
+
+
+def stop_tensorboard(process) -> None:
+    """Stops a tensorboard server.
+
+    Args:
+        process: The process of the tensorboard server.
+
+    Returns:
+        None
+
+    Examples:
+        >>> process = start_tensorboard()
+        >>> stop_tensorboard(process)
+    """
+    process.terminate()
