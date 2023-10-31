d39371a5157794f2923b937a891423396e9a95d4
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2023-10-31 16:50:04 +0100

    0.6.21
    test fun_control_init

---
+++ test/test_fun_control_init.py
@@ -0,0 +1,17 @@
+from spotPython.utils.init import fun_control_init
+
+
+def test_fun_control_init():
+    """Test fun_control_init function."""
+    fun_control = fun_control_init(_L_in=64, _L_out=11, num_workers=0, device=None)
+    assert fun_control["_L_in"] == 64
+    assert fun_control["_L_out"] == 11
+    assert fun_control["num_workers"] == 0
+    assert fun_control["device"] is None
+    assert fun_control["task"] == "classification"
+    assert fun_control["sigma"] == 0.0
+    assert fun_control["CHECKPOINT_PATH"] == "runs/saved_models/"
+    assert fun_control["DATASET_PATH"] == "data/"
+    assert fun_control["RESULTS_PATH"] == "results/"
+    assert fun_control["TENSORBOARD_PATH"] == "runs/"
+    assert fun_control["spot_tensorboard_path"] is None
