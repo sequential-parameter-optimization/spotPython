28f774fda6184f6332c39a40ecfc8fa271b31f1b
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2023-11-12 16:25:28 +0100

    pkl data added

--- test/test_pkldataset.py
+++ test/test_pkldataset.py
@@ -1,35 +1,34 @@
-# TODO: Setup test for PKLDataset that can use the pkl file form data folder
-# import pytest
-# import torch
-# from torch.utils.data import DataLoader
-# from spotPython.data.pkldataset import PKLDataset
+import pytest
+import torch
+from torch.utils.data import DataLoader
+from spotPython.data.pkldataset import PKLDataset
 
 
-# def test_pkl_dataset():
-#     # Create an instance of PKLDataset for testing
-#     dataset = PKLDataset(target_column='prognosis')
+def test_pkl_dataset():
+    # Create an instance of PKLDataset for testing
+    dataset = PKLDataset(target_column='prognosis')
 
-#     # Test the length of the dataset
-#     assert len(dataset) > 0
+    # Test the length of the dataset
+    assert len(dataset) > 0
 
-#     # Test __getitem__
-#     idx = 0
-#     sample = dataset[idx]
-#     assert isinstance(sample, tuple)
-#     assert len(sample) == 2
-#     feature, target = sample
-#     assert isinstance(feature, torch.Tensor)
-#     assert isinstance(target, torch.Tensor)
+    # Test __getitem__
+    idx = 0
+    sample = dataset[idx]
+    assert isinstance(sample, tuple)
+    assert len(sample) == 2
+    feature, target = sample
+    assert isinstance(feature, torch.Tensor)
+    assert isinstance(target, torch.Tensor)
 
-#     # Test DataLoader
-#     batch_size = 3
-#     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-#     for batch in dataloader:
-#         inputs, targets = batch
-#         assert inputs.size(0) == batch_size
-#         assert targets.size(0) == batch_size
-#         break
+    # Test DataLoader
+    batch_size = 3
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    for batch in dataloader:
+        inputs, targets = batch
+        assert inputs.size(0) == batch_size
+        assert targets.size(0) == batch_size
+        break
 
 
-# if __name__ == "__main__":
-#     pytest.main(["-v", __file__])
+if __name__ == "__main__":
+    pytest.main(["-v", __file__])
